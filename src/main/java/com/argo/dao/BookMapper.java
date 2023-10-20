8b5ad0e603f9e1e75e6a688d674c5a57094689ca
Yong Yang <yangy153@chinatelecom.cn>
2023-10-20 17:42:11 +0800

    配置数据库

--- src/main/java/com/argo/dao/BookMapper.java
+++ src/main/java/com/argo/dao/BookMapper.java
@@ -1,11 +1,13 @@
 package com.argo.dao;
 
+import com.argo.domain.Book;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author YangY
  */
 @Mapper
-public interface BookMapper {
+public interface BookMapper extends BaseMapper<Book> {
 
 }
--- src/test/java/com/argo/Ssmp1020ApplicationTests.java
+++ src/test/java/com/argo/Ssmp1020ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.argo;
 
+import com.argo.dao.BookMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,14 @@ class Ssmp1020ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    @Test
+    void testGetById(){
+        System.out.println(bookMapper.selectById(1));
     }
 
 }
